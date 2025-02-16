37d60ec0e90ac889f054de6b712a2e8c2a7e30d7
Fsociety <souhailleaders2003@gmail.com>
2025-02-16 21:03:52 +0100

    convert `class` to `function` in `sectionOne.js`

--- src/components/layouts/SectionOne.js
+++ src/components/layouts/SectionOne.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import SignUpModal from "./SignUpModal";
 import SignInModal from "./SignInModal";
 import HeaderOne from "./HeaderOne";
@@ -6,20 +6,20 @@ import PageHeaderOne from "./PageHeaderOne";
 import FooterOne from "./FooterOne";
 import BackToTop from "./BackToTop";
 
-class SectionOne extends Component {
-    render() {
+function SectionOne (props) {
+    
         return (
             <>
                 <SignUpModal />
                 <SignInModal />
                 <HeaderOne />
-                <PageHeaderOne title={this.props.title} />
-                {this.props.children}
+                <PageHeaderOne title={props.title} />
+                {props.children}
                 <FooterOne />
                 <BackToTop />
             </>
         );
     }
-}
+
 
 export default SectionOne;
