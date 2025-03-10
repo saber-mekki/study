b353fc5482c41ebcd406acb4773663aa88fbe8de
Fsociety <souhailleaders2003@gmail.com>
2025-03-10 05:22:37 +0100

    fix: update and fix `routing` in route.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Router, Route, Switch, HashRouter,Redirect} from "react-router-dom";
+import { Router, Route, Switch, HashRouter, Redirect } from "react-router-dom";
 
 import history from "./History";
 import HomeOne from "./components/HomeOne";
@@ -16,40 +16,40 @@ import CourseDetailsTwo from "./components/CourseDetailsTwo";
 import BlogDetails from "./components/BlogDetails";
 import Contact from "./components/Contact";
 
-import {ProtectedRoute,ProtectedAdminRoute} from "./ProtectedRoute";
-import SignInModal from "./components/SignInModal";
-import AdminDashboard from "./dashboard";
+import { ProtectedRoute, ProtectedAdminRoute } from "./ProtectedRoute";
+/* import SignInModal from "./components/SignInModal";
+ */import AdminDashboard from "./dashboard";
 
-function Routes  () {
+function Routes() {
 
     const isAdmin = localStorage.getItem("role") === "admin";
-        return (
-            <Router history={history}>
-                <HashRouter basename="/">
-                    <Switch>
-                        <Route exact path={'/'} render={(props) => (<HomeOne {...props} />)} />
-                        <Route exact path={'/home-one'} render={(props) => (<HomeOne {...props} />)} />
-                        <Route exact path={'/home-two'} render={(props) => (<HomeTwo {...props} />)} />
-                        <Route exact path={'/home-three'} render={(props) => (<HomeThree {...props} />)} />
-                        <Route exact path={'/about-one'} render={(props) => (<AboutOne {...props} />)} />
-                        <Route exact path={'/about-two'} render={(props) => (<AboutTwo {...props} />)} />
-                        <Route exact path={'/about-three'} render={(props) => (<AboutThree {...props} />)} />
-                        <Route exact path={'/courses'} render={(props) => (<Courses {...props} />)} />
-                        <Route exact path={'/login'} render={(props) => (<SignInModal {...props} />)} />
-                        <Route exact path={'/job-board'} render={(props) => (<JobBoard {...props} />)} />
-                        <Route exact path={'/course-details-one'} render={(props) => (<CourseDetailsOne {...props} />)} />
-                        <Route exact path={'/course-details-two'} render={(props) => (<CourseDetailsTwo {...props} />)} />
-                        <Route exact path={'/blog-details'} render={(props) => (<BlogDetails {...props} />)} />
-                        <Route exact path={'/contact'} render={(props) => (<Contact {...props} />)} />
-                 
-                        <ProtectedAdminRoute isAdmin={isAdmin} exact path="/dash/admin" component={AdminDashboard} />
-                        <ProtectedRoute exact path="/dashboard" component={Courses} />
-                        <ProtectedRoute exact path={'/blog'} component={Blog}  />
-                        <Route render={() => <Redirect to="/login" />} />
-                        </Switch>
-                </HashRouter>
-            </Router>
-        )
-    }
+    return (
+        <Router history={history}>
+            <HashRouter basename="/">
+                <Switch>
+                    <Route exact path={'/'} render={(props) => (<HomeOne {...props} />)} />
+                    <Route exact path={'/home-one'} render={(props) => (<HomeOne {...props} />)} />
+                    <Route exact path={'/home-two'} render={(props) => (<HomeTwo {...props} />)} />
+                    <Route exact path={'/home-three'} render={(props) => (<HomeThree {...props} />)} />
+                    <Route exact path={'/about-one'} render={(props) => (<AboutOne {...props} />)} />
+                    <Route exact path={'/about-two'} render={(props) => (<AboutTwo {...props} />)} />
+                    <Route exact path={'/about-three'} render={(props) => (<AboutThree {...props} />)} />
+                    <Route exact path={'/courses'} render={(props) => (<Courses {...props} />)} />
+                    <Route exact path={'/login'} render={(props) => (<HomeOne {...props} />)} />
+                    <Route exact path={'/job-board'} render={(props) => (<JobBoard {...props} />)} />
+                    <Route exact path={'/course-details-one'} render={(props) => (<CourseDetailsOne {...props} />)} />
+                    <Route exact path={'/course-details-two'} render={(props) => (<CourseDetailsTwo {...props} />)} />
+                    <Route exact path={'/blog-details'} render={(props) => (<BlogDetails {...props} />)} />
+                    <Route exact path={'/contact'} render={(props) => (<Contact {...props} />)} />
+
+                    <ProtectedAdminRoute isAdmin={isAdmin} exact path="/dash/admin" component={AdminDashboard} />
+                    <ProtectedRoute exact path="/dashboard" component={Courses} />
+                    <ProtectedRoute exact path={'/blog'} component={Blog} />
+                    <Route render={() => <Redirect to="/login" />} />
+                </Switch>
+            </HashRouter>
+        </Router>
+    )
+}
 
 export default Routes;
