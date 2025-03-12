85bb9a6a53b83d1629441d09e9900ef7b68dc1cc
Fsociety <souhailleaders2003@gmail.com>
2025-03-12 02:23:23 +0100

    fix:fix bug in `Connexion` and `pages` menu in mobile and add  translations

--- src/components/layouts/HeaderOne.js
+++ src/components/layouts/HeaderOne.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min";
 
 import LanguageDropdown from "../LanguageDropdown";
 import ProfileMenu from "./ProfileMenu";
@@ -8,6 +9,11 @@ import ProfileMenu from "./ProfileMenu";
 function HeaderOne() {
   const { t } = useTranslation();
   const isAuthenticated = localStorage.getItem("authToken");
+  useEffect(() => {
+    document.querySelectorAll(".dropdown-toggle").forEach((dropdown) => {
+      new bootstrap.Dropdown(dropdown);
+    });
+  }, []);
 
   return (
     <header className="bg-white shadow">
@@ -24,8 +30,8 @@ function HeaderOne() {
           <button
             className="navbar-toggler"
             type="button"
-            data-toggle="collapse"
-            data-target="#navbarNavAlt"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAlt"
             aria-controls="navbarNavAlt"
             aria-expanded="false"
             aria-label="Toggle navigation"
@@ -36,37 +42,63 @@ function HeaderOne() {
           <div className="collapse navbar-collapse" id="navbarNavAlt">
             <ul className="navbar-nav d-flex align-items-center ml-auto">
               <li className="nav-item">
-                <Link to={"/"} className="nav-link">{t("Home")}</Link>
+                <Link to={"/"} className="nav-link">
+                  {t("Home")}
+                </Link>
               </li>
               <li className="nav-item">
-                <Link to={"/about-one"} className="nav-link">{t("About")}</Link>
+                <Link to={"/about-one"} className="nav-link">
+                  {t("About")}
+                </Link>
               </li>
               <li className="nav-item">
-                <Link to={"/courses"} className="nav-link">{t("Courses")}</Link>
+                <Link to={"/courses"} className="nav-link">
+                  {t("Courses")}
+                </Link>
               </li>
               <li className="nav-item">
-                <Link to={"/blog"} className="nav-link">{t("Blog")}</Link>
+                <Link to={"/blog"} className="nav-link">
+                  {t("Blog")}
+                </Link>
               </li>
               <li className="nav-item dropdown">
                 <Link
                   className="nav-link dropdown-toggle"
                   to={"/"}
                   role="button"
-                  data-toggle="dropdown"
+                  data-bs-toggle="dropdown"
                   aria-haspopup="true"
                   aria-expanded="false"
                 >
                   {t("Pages")} <i className="fas fa-angle-down" />
                 </Link>
                 <ul className="dropdown-menu">
-                  <li><Link to={"/job-board"} className="dropdown-item">Job Board</Link></li>
-                  <li><Link to={"/course-details-one"} className="dropdown-item">Course Details 01</Link></li>
-                  <li><Link to={"/course-details-two"} className="dropdown-item">Course Details 02</Link></li>
-                  <li><Link to={"/blog-details"} className="dropdown-item">Blog Details</Link></li>
+                  <li>
+                    <Link to={"/job-board"} className="dropdown-item">
+                      Job Board
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={"/course-details-one"} className="dropdown-item">
+                      Course Details 01
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={"/course-details-two"} className="dropdown-item">
+                      Course Details 02
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={"/blog-details"} className="dropdown-item">
+                      Blog Details
+                    </Link>
+                  </li>
                 </ul>
               </li>
               <li className="nav-item">
-                <Link to={"/contact"} className="nav-link">{t("Contact Us")}</Link>
+                <Link to={"/contact"} className="nav-link">
+                  {t("Contact Us")}
+                </Link>
               </li>
 
               {!isAuthenticated && (
@@ -77,22 +109,40 @@ function HeaderOne() {
                       href="#!"
                       id="connexionDropdown"
                       role="button"
-                      data-toggle="dropdown"
+                      data-bs-toggle="dropdown"
                       aria-expanded="false"
                     >
-                      {t("Connexion")} <i className="fas fa-angle-down" /> 
+                      {t("Connexion")} <i className="fas fa-angle-down" />
                     </a>
-                    <div className="dropdown-menu" aria-labelledby="connexionDropdown">
-                      <a className="dropdown-item" href="#!" data-toggle="modal" data-target="#signin-modal">
+                    <div
+                      className="dropdown-menu"
+                      aria-labelledby="connexionDropdown"
+                    >
+                      <a
+                        className="dropdown-item"
+                        href="#!"
+                        data-toggle="modal"
+                        data-target="#signin-modal"
+                      >
                         {t("Sign In")}
                       </a>
-                      <a className="dropdown-item" href="#!" data-toggle="modal" data-target="#signup-modal">
+                      <a
+                        className="dropdown-item"
+                        href="#!"
+                        data-toggle="modal"
+                        data-target="#signup-modal"
+                      >
                         {t("Sign Up")}
                       </a>
                     </div>
                   </li>
                   <li className="nav-item">
-                    <a href="#!" className="btn btn-sm btn-blue rounded-pill" data-toggle="modal" data-target="#signuptutor">
+                    <a
+                      href="#!"
+                      className="btn btn-sm btn-blue rounded-pill"
+                      data-toggle="modal"
+                      data-target="#signuptutor"
+                    >
                       {t("Become A Tutor")}
                     </a>
                   </li>
