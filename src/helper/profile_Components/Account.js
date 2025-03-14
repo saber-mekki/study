80752ec06255732f3bd38edc90d6dfe80abcd82b
Fsociety <souhailleaders2003@gmail.com>
2025-03-14 06:46:44 +0100

    feat: add `date of birth` `functionality` and complete account component

--- src/helper/profile_Components/Account.js
+++ src/helper/profile_Components/Account.js
@@ -1,79 +1,167 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-export default function Account({ email }) {
+export default function Account({
+  email,
+  fullName: initialFullName,
+  phoneNumber: initialPhoneNumber,
+  date,
+  usergender,
+  onAccountUpdate
+
+}) {
   const [error, setError] = useState("");
   const [isEditing, setIsEditing] = useState(false);
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [role, setRole] = useState("");
-  const [fullName, setFullName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState(initialPhoneNumber || "");
+  const [fullName, setFullName] = useState(initialFullName || "");
+  const [Succ, SetSucc] = useState(false);
+  const [EmailError, setEmailError] = useState("");
   const [emailValue, setEmailValue] = useState(email);
+  const [EmailWarning, setEmailWarning] = useState(false);
+  const [prevEmail, setPrevEmail] = useState(email);
+  const [Code, SetCode] = useState(false);
+  const [Mycode, SetMycode] = useState('');
+  const [ErrorCode, SetErrorCode] = useState("");
+  const [ResendCode, SetResendCode] = useState(false);
+  const [resendDisabled, setResendDisabled] = useState(false);
+  const [countdown, setCountdown] = useState(60);
+  const [gender, setGender] = useState(usergender);
 
-  const fetchUserData = async (userEmail) => {
-    try {
-      const response = await axios.post(
-        "http://localhost:5000/api/v1/getUser",
-        {
-          email: emailValue,
-        }
-      );
 
-      if (response.data.error) {
-        setError("User not found");
-      } else {
-        const user = response.data.user;
-        setFullName(user.user_name);
-        setRole(user.type_register);
-        setPhoneNumber(user.phone_number);
-        setEmailValue(user.user_email);
-      }
-    } catch (err) {
-      setError("An error occurred while fetching user data.");
+  const [birthDate, setBirthDate] = useState(() => {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = (d.getMonth() + 1).toString().padStart(2, '0');
+    const day = d.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  });
+
+  const [dateError, setDateError] = useState("");
+
+
+
+
+  const handleDateChange = (e) => {
+    const selectedDate = new Date(e.target.value);
+    const today = new Date();
+    const minAgeDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
+
+    if (selectedDate > minAgeDate) {
+      setDateError("You must be at least 18 years old.");
+      setError('')
+    } else {
+      setDateError("");
     }
+
+    setBirthDate(e.target.value);
   };
 
-  useEffect(() => {
-    fetchUserData(emailValue);
-  }, []);
 
-  const handleConfirm = async () => {
+  const handleModalClose = () => {
+    SetCode(false);
+    SetMycode('')
+    SetErrorCode("")
+    setEmailWarning(false);
+  };
+  const handleConfirm = () => {
+    SetSucc("");
+    if (!fullName || !emailValue || !phoneNumber) {
+      setError("All fields are required.");
+      return;
+    }
+    else {
+      setError("")
+    }
+
+
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailPattern.test(emailValue)) {
+      setEmailError("Please enter a valid email.");
+      return;
+    }
+
+    if (emailValue !== prevEmail) {
+      setEmailWarning(true);
+      return;
+    }
+
+    updateUser();
+
+  
+  };
+
+  const updateUser = async () => {
     try {
       const response = await axios.post(
         "http://localhost:5000/api/v1/updateUser",
         {
           name: fullName,
-          email,
+          email: prevEmail,
           newEmail: emailValue,
-          type_register: role,
+          type_register: "student",
+          gender: gender,
+          date_of_birth: birthDate,
           phone_number: phoneNumber,
         }
       );
-
+      const token = response.data.tokens.accessToken;
+      localStorage.setItem("authToken", token);
       if (response.data.error) {
         setError(response.data.message || "Error updating user");
-        console.error(
-          "Error from API:",
-          response.data.message || "Unknown error"
-        );
         return;
       }
 
       setIsEditing(false);
-      alert("User details updated successfully!");
 
-      await fetchUserData(emailValue);
-      alert(emailValue);
+      SetSucc(true);
+      setPrevEmail(emailValue);
     } catch (err) {
       setError("An error occurred while updating user details.");
       console.error("Error during the update process:", err);
     }
+
   };
 
   const handleCancel = () => {
-    setFullName(fullName);
-    setEmailValue(email);
-    setPhoneNumber(phoneNumber);
+    setFullName(initialFullName);
+    setEmailValue(prevEmail);
+    setPhoneNumber(initialPhoneNumber);
     setIsEditing(false);
+    setDateError('')
+  };
+  const HandleConfirmCode = () => {
+    if (Mycode === "0000") {
+      SetCode(false);
+      updateUser();
+    } else {
+      SetResendCode(true);
+      SetErrorCode("Invaalid Code");
+    }
+  };
+
+  const handleCancelEmailChange = () => {
+    setEmailWarning(false);
+  };
+
+  const handleConfirmEmailChange = () => {
+    setEmailWarning(false);
+    SetCode(true);
+  };
+  const handleResendCode = () => {
+    SetErrorCode("Wait 60s before requesting a new code ⏳");
+    setResendDisabled(true);
+    setCountdown(60);
+
+    const timer = setInterval(() => {
+      setCountdown((prev) => {
+        if (prev === 1) {
+          clearInterval(timer);
+          setResendDisabled(false);
+          return 60;
+        }
+        return prev - 1;
+      });
+    }, 1000);
   };
 
   return (
@@ -81,9 +169,8 @@ export default function Account({ email }) {
       <h6 className="mb-2 text-primary">Personal Details</h6>
 
       {error && <div className="alert alert-danger">{error}</div>}
-
       <div className="row gutters">
-        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
           <div className="form-group">
             <label htmlFor="fullName">Full Name</label>
             <input
@@ -97,7 +184,7 @@ export default function Account({ email }) {
             />
           </div>
         </div>
-        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
           <div className="form-group">
             <label htmlFor="eMail">Email</label>
             <input
@@ -110,8 +197,9 @@ export default function Account({ email }) {
               disabled={!isEditing}
             />
           </div>
+          {EmailError && <div className="alert alert-danger">{EmailError}</div>}
         </div>
-        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
           <div className="form-group">
             <label htmlFor="phone">Phone</label>
             <input
@@ -125,7 +213,241 @@ export default function Account({ email }) {
             />
           </div>
         </div>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
+          <div className="form-group">
+            <label>Gender</label>
+            <div className="d-flex custom-radio-group rounded-sm">
+              <div className="custom-control custom-radio">
+                <input
+                  type="radio"
+                  id="customRadioMale"
+                  name="gender"
+                  className="custom-control-input"
+                  value="male"
+                  onChange={(e) => setGender(e.target.value)}
+                  checked={gender === "male"}
+                />
+                <label
+                  className="custom-control-label"
+                  htmlFor="customRadioMale"
+                >
+                  {("male")}
+                </label>
+              </div>
+              <div className="custom-control custom-radio">
+                <input
+                  type="radio"
+                  id="customRadioFemale"
+                  name="gender"
+                  className="custom-control-input"
+                  value="female"
+                  onChange={(e) => setGender(e.target.value)}
+                  checked={gender === "female"}
+
+
+                />
+                <label
+                  className="custom-control-label"
+                  htmlFor="customRadioFemale"
+                >
+                  {("female")}
+                </label>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-3">
+          <div className="form-group">
+            <label htmlFor="birthDate">Date of Birth</label>
+            <input
+              type="date"
+              className="form-control"
+              id="birthDate"
+              value={birthDate}
+              onChange={handleDateChange}
+              required
+              disabled={!isEditing}
+            />
+            {dateError && <div className="alert alert-danger">{dateError}</div>}
+          </div>
+        </div>
       </div>
+
+
+
+
+      {Succ && (
+        <div
+          id="succ"
+          onClick={(e) => {
+            if (e.target.id === "succ") onAccountUpdate();
+          }}
+          className="modal fade show d-block"
+          tabIndex="-1"
+          role="dialog"
+        >
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content p-4" style={{ borderRadius: '12px', borderColor: '#6c757d' }}>
+              <div className="modal-body text-center">
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  onClick={onAccountUpdate}
+                  aria-label="Close"
+                  style={{ color: '#6c757d' }}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+
+                <i className="bi bi-check-circle text-success fs-1 mb-3"></i>
+                <h4 className="text-success mb-3" style={{ fontWeight: 'bold' }}>
+                Details Updated Successfully!
+                </h4>
+                <h6 className="text-muted mb-4">
+                  Your Details Have Been Updated Successfully!
+                </h6>
+                <div className="d-flex justify-content-center">
+                  <button
+                    onClick={onAccountUpdate}
+                    type="button"
+                    className="btn btn-success mt-2"
+                    style={{
+                      padding: '10px 20px',
+                      fontSize: '16px',
+                      fontWeight: 'bold',
+                      borderRadius: '8px',
+                      boxShadow: '0 4px 6px rgba(0,0,0,0.1)',
+                    }}
+                  >
+                    Continue
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+
+
+
+
+
+
+      {(EmailWarning || Code) && (
+        <div
+          id="CodeConf"
+          onClick={(e) => {
+            if (e.target.id === "CodeConf") handleModalClose();
+          }}
+          className="modal fade show d-block"
+          tabIndex="-1"
+          role="dialog"
+        >
+          <div className="modal-dialog modal-dialog-centered" role="document">
+
+            <div className="modal-content p-3">
+
+              <div className="modal-body text-center">
+                <button
+                  type="button"
+                  className="close  "
+                  data-dismiss="modal"
+                  onClick={handleModalClose}
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+
+
+                {EmailWarning && (
+                  <>
+                    <i className="bi bi-exclamation-circle text-warning fs-1 mb-3"></i>
+                    <h4 className="text-warning mb-3">Confirm Email Change</h4>
+                    <h6 className="text-muted mb-3">
+                      Are you sure you want to change your email? {prevEmail} to{" "}
+                      {emailValue}
+                    </h6>
+                    <button
+                      onClick={handleConfirmEmailChange}
+                      type="button"
+                      className="btn btn-success mt-2 me-2"
+                    >
+                      Yes
+                    </button>
+                    <button
+                      onClick={handleCancelEmailChange}
+                      type="button"
+                      className="btn btn-danger mt-2"
+                    >
+                      No
+                    </button>
+                  </>
+                )}
+
+                {Code && (
+                  <form className="row" onSubmit={(e) => e.preventDefault()}>
+                    <div className="form-group mb-3 col-12">
+                      <label className="text-secondary h6 mb-2" htmlFor="code">
+                        Confirmation Code
+                      </label>
+                      <input
+                        onChange={(e) => {
+                          const value = e.target.value.replace(/\D/g, "");
+                          if (value.length <= 4) {
+                            SetMycode(value);
+                          }
+                          SetErrorCode("");
+                        }}
+                        value={Mycode}
+                        placeholder="####"
+                        className="form-control shadow-none rounded-sm text-center"
+                        type="number"
+                        id="code"
+                        required
+                        maxLength="4"
+                      />
+                    </div>
+
+                    {ErrorCode && (
+                      <h6 className="alert bg-white text-danger mx-auto">
+                        {ErrorCode}
+                      </h6>
+                    )}
+
+                    <div className="form-group col-12">
+                      <button
+                        className="btn btn-primary w-100 rounded-sm"
+                        type="button"
+                        onClick={HandleConfirmCode}
+                        disabled={Mycode.length !== 4}
+                      >
+                        Verify Code
+                      </button>
+
+                      {ResendCode && (
+                        <button
+                          className="btn btn-success w-100 my-2 rounded-sm"
+                          type="button"
+                          onClick={handleResendCode}
+                          disabled={resendDisabled}
+                        >
+                          {resendDisabled
+                            ? `Wait ${countdown}s`
+                            : "Resend Code"}
+                        </button>
+                      )}
+                    </div>
+                  </form>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
 
       {!isEditing ? (
         <button
