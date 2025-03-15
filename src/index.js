e16eb2a77393b6bb986e2d3231be5cbf08daedab
saber-mekki <saber.mekki6@gmail.com>
2025-03-15 22:58:54 +0100

    fix: wrap `app` with `provider` in `src` index file

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
+import  store from "./redux/store";
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
