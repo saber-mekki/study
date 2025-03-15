4a7a2ecec69df318dc753a91975c60deedbbb1f0
saber-mekki <saber.mekki6@gmail.com>
2025-03-15 22:59:52 +0100

    eat: add `redux` config

---
+++ src/redux/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  name: "",
+  role: "",
+  phone: "",
+  dateOfBirth: "",
+  gender: "male",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.name = action.payload.name;
+      state.role = action.payload.role;
+      state.phone = action.payload.phone;
+      state.dateOfBirth = action.payload.dateOfBirth;
+      state.gender = action.payload.gender;
+    },
+    updateUserStore: (state, action) => {
+      return { ...state, ...action.payload };
+    },
+  },
+});
+
+export const { setUser, updateUser } = userSlice.actions;
+export default userSlice.reducer;
