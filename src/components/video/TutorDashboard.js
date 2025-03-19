5d4bb56912f6849346a2a6b5faa70841cc14ffe3
saber-mekki <saber.mekki6@gmail.com>
2025-03-19 15:38:08 +0100

    fix: fix `socket url`

--- src/components/video/TutorDashboard.js
+++ src/components/video/TutorDashboard.js
@@ -1,8 +1,8 @@
 import { useState, useRef, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
-const peerConnections = {}; // Store peer connections
+const socket = io(`${process.env.REACT_APP_API_URL}`);
+const peerConnections = {}; 
 
 const TutorLiveSession = () => {
   const [roomCode, setRoomCode] = useState("");
