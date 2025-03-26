275bc4abf352dccdb5d3062f01591432b7bba347
Fsociety <souhailleaders2003@gmail.com>
2025-03-26 03:30:38 +0100

    fix : fix courses.js

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,26 +1,14 @@
-import React, {Component} from "react";
+import React from "react";
 import SectionTwo from "./layouts/SectionTwo";
 import CourseSection from "./layouts/CourseSection";
 
-class Courses extends Component {
-    render() {
-        return (
-            <SectionTwo title={'Courses'}>
-                {/* Courses Section imported from layouts */}
-                <CourseSection />
+export default function Courses() {
 
-                {/*<nav className="mt-70">*/}
-                {/*    <ul className="pagination justify-content-center font-weight-600">*/}
-                {/*        <li className="page-item"><Link to={'/'} className="page-link"><i className="fas fa-chevron-left" /></Link></li>*/}
-                {/*        <li className="page-item"><Link to={'/'} className="page-link">01</Link></li>*/}
-                {/*        <li className="page-item active"><Link to={'/'} className="page-link">02</Link></li>*/}
-                {/*        <li className="page-item"><Link className="page-link">03</Link></li>*/}
-                {/*        <li className="page-item"><Link to={'/'} className="page-link"><i className="fas fa-chevron-right" /></Link></li>*/}
-                {/*    </ul>*/}
-                {/*</nav>*/}
-            </SectionTwo>
-        );
-    }
+ 
+
+  return (
+    <SectionTwo title={'Courses'}>
+      <CourseSection  />
+    </SectionTwo>
+  );
 }
-
-export default Courses;
