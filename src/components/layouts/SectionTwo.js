46b39b68ff3625d72daf5cf50c2d3af777ab89b2
Fsociety <souhailleaders2003@gmail.com>
2025-04-05 21:42:44 +0100

    feat:change HeaderTwo with HeaderOne in SectionTwo

--- src/components/layouts/SectionTwo.js
+++ src/components/layouts/SectionTwo.js
@@ -1,11 +1,12 @@
 import React, {Component} from "react";
 import SignUpModal from "./SignUpModal";
 import SignInModal from "./SignInModal";
-import HeaderTwo from "./HeaderTwo";
-import PageHeaderTwo from "./PageHeaderTwo";
+    /* import HeaderTwo from "./HeaderTwo";
+ */import PageHeaderTwo from "./PageHeaderTwo";
 import FooterOne from "./FooterOne";
 import BackToTop from "./BackToTop";
 import ForgetPassword from "./ForgetPassword";
+import HeaderOne from "./HeaderOne";
 
 class SectionTwo extends Component {
     render() {
@@ -14,7 +15,7 @@ class SectionTwo extends Component {
              <ForgetPassword />
                 <SignUpModal />
                 <SignInModal />
-                <HeaderTwo />
+                <HeaderOne />
                 <PageHeaderTwo title={this.props.title} />
                 {this.props.children}
                 <FooterOne />
