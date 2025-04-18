9ec92f89c11fa0f21ee2bc1309bb438ad347c6ce
Fsociety <souhailleaders2003@gmail.com>
2025-04-19 00:15:41 +0100

    feat:add status to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Routes from "./Routes";
+import Status from './Status';
 
 function App() {
   return (
+    <>
+    <Status/>
       <Routes />
+    </>
   );
 }
 
