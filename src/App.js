cdc8bc6ee65eb28ac4445ed4e8f1b74e372fe68c
saber-mekki <saber.mekki6@gmail.com>
2025-08-31 14:12:23 +0200

    chore: clean-up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Routes from "./Routes";
-import Status from './Status';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
-    <Status/>
+    
       <Routes />
       <ToastContainer position="top-center" autoClose={4000} hideProgressBar={false} />
     </>
